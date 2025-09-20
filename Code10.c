0204dd90d793cdba347b52a8360b7259b9a48862
bitsAndRaghav <raghavshinghal2007@gmail.com>
2025-09-20 10:54:29 +0530

    100DaysOfCode-Code10

---
+++ Code10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int total, h, m, s;
+
+    printf("Input seconds: ");
+    if (scanf("%d", &total) != 1) 
+    {
+        return 1;
+    }
+
+    if (total < 0) 
+        total = -total;
+
+    h = total / 3600;         
+    m = (total % 3600) / 60;       
+    s = total % 60;                
+
+    printf("%02d:%02d:%02d\n", h, m, s);
+    return 0;
+}
