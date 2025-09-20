0d1a3add8f3629e1d09487eb70bd96d886a3d01b
bitsAndRaghav <raghavshinghal2007@gmail.com>
2025-09-20 10:53:52 +0530

    100DaysOfCode-Code06

---
+++ Code06.c
@@ -0,0 +1,19 @@
+//Write a program to swap two numbers using a third variable.
+
+#include<stdio.h>
+
+void main() {
+    int a=0.0, b=0.0, c=0.0;
+    printf("Enter two numbers: ");
+    scanf("%d%d", &a, &b);
+    
+    printf("\nBefore swapping: ");
+    printf("\na = %d", a);
+    printf("\nb = %d", b);
+    c=a;
+    a=b;
+    b=c;
+    printf("\nAfter swapping: ");
+    printf("\na = %d", a);
+    printf("\nb = %d", b);
+}
