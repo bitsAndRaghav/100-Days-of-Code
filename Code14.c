3cc1bb7b58867c96b2fcc5421dd20e9e9a993ed4
bitsAndRaghav <raghavshinghal2007@gmail.com>
2025-09-20 14:24:13 +0530

    100DaysOfCode-Code14

---
+++ Code14.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+        if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ||
+            ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            printf("Vowel\n");
+        } else {
+            printf("Consonant\n");
+        }
+    } else {
+        printf("Not an alphabet\n");
+    }
+
+    return 0;
+}
