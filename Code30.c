e0259c38b90a0249cfe2de04ec46b9c8f3d2cea0
bitsAndRaghav <raghavshinghal2007@gmail.com>
2025-09-21 13:38:55 +0530

    100DDaysOfCode-Code30

---
+++ Code30.c
@@ -0,0 +1,20 @@
+//Q30. Write a program to reverse a given number.
+
+#include<stdio.h>
+
+void main()
+{
+    int num=0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("Original = %d", num);
+
+    int remainder, reverse=0;
+    while(num != 0)
+    {
+        remainder=num%10;
+        reverse=reverse*10 + remainder;
+        num=num/10;
+    }
+    printf("\nReverse = %d", reverse);
+}
