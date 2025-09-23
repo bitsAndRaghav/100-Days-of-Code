bc334b1355b8d22dfbd6700dbf498b2f0cda1c53
bitsAndRaghav <raghavshinghal2007@gmail.com>
2025-09-23 11:05:31 +0530

    100DaysOfCode-Code41

---
+++ Code41.c
@@ -0,0 +1,31 @@
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int num, firstDigit, lastDigit, digits, middlePart, swappedNum;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    int temp = num;
+
+    lastDigit = temp % 10;
+
+    digits = 0;
+    while (temp != 0) {
+        firstDigit = temp; 
+        temp /= 10;
+        digits++;
+    }
+
+    middlePart = num % (int)pow(10, digits - 1);  
+    middlePart /= 10;                             
+
+    swappedNum = lastDigit * (int)pow(10, digits - 1) + middlePart * 10 + firstDigit;
+
+    printf("Number after swapping first and last digit = %d\n", swappedNum);
+
+    return 0;
+}
