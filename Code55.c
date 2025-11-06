45ea229e83bb264842a68b5bc9ca2b91528a9151
bitsAndRaghav <raghavshinghal2007@gmail.com>
2025-11-07 00:03:20 +0530

    100DaysOfCode-Code55

---
+++ Code55.c
@@ -0,0 +1,24 @@
+// Q55. Write a program to print all the prime numbers from 1 to n.
+
+#include<stdio.h>
+
+int main() {
+    int n, i, j, flag;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for(i = 2; i <= n; i++) {
+        flag = 1;
+        for(j = 2; j <= i / 2; j++) {
+            if(i % j == 0) {
+                flag = 0;
+                break;
+            }
+        }
+        if(flag == 1) {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
