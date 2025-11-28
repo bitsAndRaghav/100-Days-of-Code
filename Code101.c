f49d189924f439ac2eb1e21e161330e139b77b1f
bitsAndRaghav <raghavshinghal2007@gmail.com>
2025-11-29 00:27:01 +0530

    100DaysOfCode-Code101

--- Code101.c
+++ Code101.c
@@ -1,35 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
 
-int first_occurrence(int a[], int n, int x){
-    int l=0, r=n-1, ans=-1;
-    while(l<=r){
-        int m = l + (r-l)/2;
-        if(a[m]==x){ ans=m; r=m-1; }
-        else if(a[m]<x) l=m+1;
-        else r=m-1;
+int main() {
+    int n, i, target;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int nums[100];
+
+    printf("Enter sorted array elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
     }
-    return ans;
-}
-int last_occurrence(int a[], int n, int x){
-    int l=0, r=n-1, ans=-1;
-    while(l<=r){
-        int m = l + (r-l)/2;
-        if(a[m]==x){ ans=m; l=m+1; }
-        else if(a[m]<x) l=m+1;
-        else r=m-1;
+
+    printf("Enter target: ");
+    scanf("%d", &target);
+
+    int first = -1, last = -1;
+
+    for (i = 0; i < n; i++) {
+        if (nums[i] == target) {
+            first = i;
+            break;
+        }
     }
-    return ans;
-}
-int main(){
-    int n;
-    if(scanf(\"%d\", &n)!=1) return 0;
-    int *a = malloc(n * sizeof(int));
-    for(int i=0;i<n;i++) scanf(\"%d\", &a[i]);
-    int x; scanf(\"%d\", &x);
-    int f = first_occurrence(a,n,x);
-    int l = last_occurrence(a,n,x);
-    if(f==-1) printf(\"-1 -1\n\"); else printf(\"%d %d\n\", f, l);
-    free(a);
+
+    for (i = n - 1; i >= 0; i--) {
+        if (nums[i] == target) {
+            last = i;
+            break;
+        }
+    }
+
+    if (first == -1) {
+        printf("-1 -1\n");
+    } else {
+        printf("%d %d\n", first, last);
+    }
+
     return 0;
 }
